805b3bfbdd39ea6ab83dff945d1b9a027a24d5e3
4ertiluga <134250906+4ertiluga@users.noreply.github.com>
2023-05-22 06:46:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+//1p
+
+var basePrice = 43000;
+document.getElementById("price1").innerHTML = basePrice;
+// Функция добавления нового пассажира
+function addPassenger() {
+ 	var Plus = 43000;
+ 	basePrice = basePrice + Plus;
+document.getElementById("price1").innerHTML = basePrice;
+  }
+function removePassenger() {
+ 	if (basePrice > 0){
+ 	var Plus = 43000;
+ 	basePrice = basePrice - Plus;
+ 	document.getElementById("price1").innerHTML = basePrice;
+ 	}
+ 	else{
+document.getElementById("price1").innerHTML = basePrice;
+ 	}
+}
+
+var basePrice2 = 55000;
+document.getElementById("price2").innerHTML = basePrice2;
+// Функция добавления нового пассажира
+function addPassenger2() {
+ 	var Plus = 55000;
+ 	basePrice2 = basePrice2 + Plus;
+document.getElementById("price2").innerHTML = basePrice2;
+  }
+function removePassenger2() {
+ 	if (basePrice2 > 0){
+ 	var Plus = 55000;
+ 	basePrice2 = basePrice2 - Plus;
+ 	document.getElementById("price2").innerHTML = basePrice2;
+ 	}
+ 	else{
+document.getElementById("price2").innerHTML = basePrice2;
+ 	}
+}
+
+
+var basePrice3 = 46000;
+document.getElementById("price3").innerHTML = basePrice3;
+// Функция добавления нового пассажира
+function addPassenger3() {
+ 	var Plus = 46000;
+ 	basePrice3 = basePrice3 + Plus;
+document.getElementById("price3").innerHTML = basePrice3;
+  }
+function removePassenger3() {
+ 	if (basePrice3 > 0){
+ 	var Plus = 46000;
+ 	basePrice3 = basePrice3 - Plus;
+ 	document.getElementById("price3").innerHTML = basePrice3;
+ 	}
+ 	else{
+document.getElementById("price3").innerHTML = basePrice3;
+ 	}
+}
+
+
+
+
+ 
